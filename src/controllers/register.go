dbbdc8fac06825db705d3ff97c3ea7c14ed05d4f
Jianhai Luan <luanjianhai@gmail.com>
2017-02-26 18:57:19 +0800

    Debug api/user/register

--- src/controllers/register.go
+++ src/controllers/register.go
@@ -1,13 +1,41 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+    "fmt"
+    "encoding/json"
+
+    "github.com/astaxie/beego"
+    "../models"
+)
 
 type RegisterController struct {
     beego.Controller
 }
 
-func init() {
+func registerUser() models.User {
+   user := models.User {
+   }
+
+   return user
 }
 
 func (this *RegisterController) Post() {
+    var request map[string] interface{}
+
+    if err := json.Unmarshal(this.Ctx.Input.RequestBody, &request); err != nil {
+        this.Data["json"] = models.Respond {
+            ResultCode:	1,
+	    Message:	"Request is NULL",
+        }
+        this.ServeJSON()
+    }
+    fmt.Println(request)
+
+    data := registerUser()
+    this.Data["json"] = models.Respond {
+        ResultCode:	0,
+	Message:	"Success",
+        Data:		data,
+    }
+    this.ServeJSON()
 }
---
+++ src/models/models.go
@@ -0,0 +1,52 @@
+package models
+
+/*
+ */
+type Client struct {
+    AppId  string  `json:"appId"`
+    AppVersion  string  `json:"appVersion"`
+    UserId  int64  `json:"userId"`
+    Os  string  `json:"os"`
+    DeviceId  string  `json:"deviceId"`
+    Longitude  int64  `json:"longitude"`
+    Latitude   int64  `json:"latitude"`
+}
+
+/*
+ */
+type Topic struct {
+    TopicId  int64 `json:"topicId"`
+    Title string `json:"title"`
+}
+
+/*
+ */
+type User struct {
+    UserId  int64  `json:userId`
+    Sex  int64  `json:sex`
+    NickName  string  `json:"nickName"`
+    HeadURL  string  `json:"headURL"`
+    Birthday  string  `json:"birthday"`
+    IsOnline  int64  `json:"isOnline"`
+    Longitude  int64  `json:"longitude"`
+    Latitude   int64  `json:"latitude"`
+    LivingId   string  `json:"livingId"`
+    LivingState  int64  `json:"livingState"`
+    LivingDuration  int64  `json:"livingDuration"`
+    Golden  int64  `json:"golden"`
+    Gifts  int64  `json:"gifts"`
+    WantSex  int  `json:"wantSex"`
+    Topic   int  `json:"topic"`
+}
+
+/*
+ */
+type Giving struct {
+    GiveId  int64  `json:"giveId"`
+    Type  int64  `json:"type"`
+    Sex  int64  `json:"sex"`
+    Title  string  `json:"title"`
+    ImageURL  string  `json:"imageURL"`
+    GifURL  string  `json:"gifURL"`
+    Price  int `json:"price"`
+}
